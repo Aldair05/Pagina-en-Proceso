445538a873e7159e7fc82d71b243ff3f68186f80
aldair05 <aldair.uaem@gmail.com>
2019-02-13 13:38:50 -0600

    Agrego Portafolio

--- js/scrip.js
+++ js/scrip.js
@@ -5,3 +5,34 @@
             $("#menu").removeClass("bg-primary");
         }
       });
+
+/* (function(){
+    $(document).ready(function(){
+        $(".filter").(function(){
+            var filtro=$(this).attr("data-rel");
+
+            if(filtro=="all"){
+                $(".galeria").show(500);
+            } else{
+                $(".galeria").not("."+filtro).hide(500);
+                $(".galeria").filter("."+filtro).show(500);
+            }
+        });
+
+    });
+})
+ */
+
+
+$(function() {
+    var selectedClass = "";
+    $(".filter").click(function(){
+    selectedClass = $(this).attr("data-rel");
+            $("#galeria").fadeTo(100, 0.5);
+            $("#galeria div").not("."+selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function() {
+            $("."+selectedClass).fadeIn().addClass('animation');
+            $("#galeria").fadeTo(300, 1);
+    }, 300);
+});
+}); 
